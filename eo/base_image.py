e6badba05a59fcd621b23ccdbd09e5a6fd1abd0a
Miguel Imperial <migimperial@yahoo.com>
2025-06-03 15:26:55 +0800

    Add plotting methods in base_image.py

--- eo/base_image.py
+++ eo/base_image.py
@@ -1,6 +1,7 @@
 import xarray as xr
 from typing import Dict
 import numpy as np
+import matplotlib.pyplot as plt
 
 class BaseImage:
     def __init__(
@@ -32,8 +33,28 @@ class BaseImage:
     
 
     @staticmethod
-    def plot_histogram_with_percentiles(band_array):
-        pass
+    def _plot_histogram_with_percentiles(
+        band_name, band_array, 
+        lower:float, upper:float, figsize:tuple,
+        out_file:str
+    ):
+        p_low, p_high = np.percentile(band_array, (lower, upper))
+        plt.figure(figsize=figsize)
+        plt.hist(band_array.values.ravel(), bins=100, color='gray', alpha=0.7)
+        plt.axvline(p_low, color='red', linestyle='--', label=f'{lower}% ({p_low:.1f})')
+        plt.axvline(p_high, color='green', linestyle='--', label=f'{upper}% ({p_high:.1f})')
+        plt.title(f'{band_name} Histogram with {lower}% and {upper}% Percentiles')
+        plt.legend()
+        plt.savefig(out_file)
+
+
+    def plot_histogram_with_percentiles(self):
+        for name, band in self.bands.items():
+            self._plot_histogram_with_percentiles(
+                name, band, 
+                self.lower_percentile, self.upper_percentile, figsize=(8,4),
+                out_file=f"data/misc/{name}_{self.lower_percentile}_{self.upper_percentile}.png"
+                )
     
     
     def stretch_contrast(self) -> Dict:
@@ -44,4 +65,3 @@ class BaseImage:
         }
 
         return stretched
-    
