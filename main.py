47c9618f278117fa9abf4d7fb14160c2f2591d6d
Miguel Imperial <migimperial@yahoo.com>
2025-06-09 18:48:17 +0800

    Add function to get multiple images within a date range in main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import rioxarray
 import numpy as np
 from eo.base_image_collection import BaseImageCollection
 from eo.base_image import BaseImage
-from eo.image_utils import search_catalog, get_best_image, get_individual_bands
+from eo.image_utils import search_catalog, get_best_image, get_best_images, get_individual_bands
 from eo.utils import set_up_dask, load_config
 
 config = load_config('config.yaml')
@@ -14,6 +14,7 @@ DTYPE_MAP = {
     'float32': np.float32,
 }
 
+PROCESSED_IMG_DIR = 'data/processed'
 BANDS_SELECTION = config['bands']
 SLICE = slice(config['slice_start'], config['slice_end'])
 LOWER_PERC = config['lower_percentile']
@@ -55,6 +56,26 @@ def run_tasks_single(**kwargs):
     image.get_rgb_stack(export=out_file)
 
 
+def run_tasks_multi(**kwargs):
+    out_file = kwargs.get('out_file')
+    best_images = get_best_images(IMAGE_RESULTS)
+    processed_images = {}
+
+    for image in best_images:
+        rgb_bands = get_individual_bands(image, BANDS_SELECTION, subset=SLICE)
+
+        processed_image = (
+            BaseImage(bands=rgb_bands, lower=LOWER_PERC, upper=UPPER_PERC, no_data_value=NO_DATA_VAL)
+            .stretch_contrast()
+            .stack_bands(['red', 'green', 'blue'])
+            .process_stack(max_val=MAX_VAL, gamma=GAMMA_CORRECTION, type=BIT_DEPTH, chunk=CHUNK_SIZE)
+        )
+
+        processed_images[image.id] = processed_image
+
+    for name, image in processed_images.items():
+        image.get_rgb_stack(export=f"{PROCESSED_IMG_DIR}/{name}.tif")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description=("Get the best image based on XY and date range"),
@@ -79,6 +100,10 @@ if __name__ == "__main__":
     if mode == 'single':
         set_up_dask(enabled=True)
         run_tasks_single(out_file=out)
+
+    elif mode == 'multi':
+        set_up_dask(enabled=True)
+        run_tasks_multi(out_file=out)
 
     elif mode == 'hist':
         BaseImage._plot_histogram_with_percentiles(
