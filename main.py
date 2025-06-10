85b8483b01ead05e4b04f62e0c27fe3e7009686b
Miguel Imperial <migimperial@yahoo.com>
2025-06-10 18:47:59 +0800

    Allow stacking RGB or using visual asset in single mode

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ if __name__ == "__main__":
     parser.add_argument("--bn", required=False, type=str)
     parser.add_argument("--lo", required=False, type=str)
     parser.add_argument("--up", required=False, type=str)
+    parser.add_argument("--type", required=False, type=str)
     args = parser.parse_args()
 
     mode = args.mode
@@ -41,10 +42,12 @@ if __name__ == "__main__":
     bn = args.bn
     lo = args.lo
     up = args.up
+    up = args.up
+    type = args.type
 
     if mode == 'single':
         set_up_dask(enabled=True)
-        single.run(image_selection=IMAGE_RESULTS, out_file=out)
+        single.run(image_selection=IMAGE_RESULTS, out_file=out, type=type)
 
     elif mode == 'multi':
         set_up_dask(enabled=True)
