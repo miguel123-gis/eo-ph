25fe75256e5570f6b5db1b8b9a32ab0c736be157
Miguel Imperial <migimperial@yahoo.com>
2025-06-10 15:34:31 +0800

    Create a separate module for running multi mode

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import rioxarray
 import numpy as np
 from eo.base_image_collection import BaseImageCollection
 from eo.base_image import BaseImage
-from eo.image_utils import search_catalog, get_best_images, get_individual_bands
+from eo.image_utils import search_catalog
 from eo.utils import set_up_dask, load_config
-from eo.modes import single
+from eo.modes import single, multi
 
 CONFIG = load_config('config.yaml')
 
@@ -21,14 +21,6 @@ START_DATE = CONFIG['start_date']
 END_DATE = CONFIG['end_date']
 LONGITUDE = float(CONFIG['longitude'])
 LATITUDE = float(CONFIG['latitude'])
-SLICE = slice(CONFIG['slice_start'], CONFIG['slice_end'])
-LOWER_PERC = CONFIG['lower_percentile']
-UPPER_PERC = CONFIG['upper_percentile']
-NO_DATA_VAL = CONFIG['no_data_value']
-MAX_VAL = CONFIG['maximum_value']
-BIT_DEPTH = DTYPE_MAP.get(CONFIG['bit_depth'])
-GAMMA_CORRECTION = CONFIG['gamma_correction']
-CHUNK_SIZE = CONFIG['chunk_size']
 
 IMAGE_COLLECTION = BaseImageCollection(
     start_date = START_DATE,
@@ -39,26 +31,6 @@ IMAGE_COLLECTION = BaseImageCollection(
 )
 
 IMAGE_RESULTS = search_catalog(IMAGE_COLLECTION)
-
-def run_tasks_multi(**kwargs):
-    out_file = kwargs.get('out_file')
-    best_images = get_best_images(IMAGE_RESULTS)
-    processed_images = {}
-
-    for image in best_images:
-        rgb_bands = get_individual_bands(image, BANDS_SELECTION, subset=SLICE)
-
-        processed_image = (
-            BaseImage(bands=rgb_bands, lower=LOWER_PERC, upper=UPPER_PERC, no_data_value=NO_DATA_VAL)
-            .stretch_contrast()
-            .stack_bands(['red', 'green', 'blue'])
-            .process_stack(max_val=MAX_VAL, gamma=GAMMA_CORRECTION, type=BIT_DEPTH, chunk=CHUNK_SIZE)
-        )
-
-        processed_images[image.id] = processed_image
-
-    for name, image in processed_images.items():
-        image.get_rgb_stack(export=f"{PROCESSED_IMG_DIR}/{name}.tif")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -88,7 +60,7 @@ if __name__ == "__main__":
 
     elif mode == 'multi':
         set_up_dask(enabled=True)
-        run_tasks_multi(out_file=out)
+        multi.run(image_selection=IMAGE_RESULTS)
 
     elif mode == 'hist':
         BaseImage._plot_histogram_with_percentiles(
