696834f5c8492a6da872b1d776b0371fc3173a95
Miguel Imperial <migimperial@yahoo.com>
2025-11-28 22:51:22 +0800

    Transfer all logic to collect and export images in a single class

--- eo/__main__.py
+++ eo/__main__.py
@@ -1,33 +1,23 @@
 import argparse
-import time
 from pathlib import Path
 from eo.logger import logger
-from eo.base_image_collection import BaseImageCollection
-from eo.image_utils import search_catalog
 from eo.utils import load_config
-from eo.modes import basic
+from eo.modes.basic import BasicMode
 
 PROJECT_DIR = Path(__file__).resolve().parent.parent
-CONFIG = load_config('config.yaml')
+CONFIG = load_config('config.yaml') # TODO Add handle if config.yaml is not existing
 
 START_DATE = CONFIG['start_date']
 END_DATE = CONFIG['end_date']
 LONGITUDE = float(CONFIG['longitude'])
 LATITUDE = float(CONFIG['latitude'])
 BUFFER = float(CONFIG['buffer_size_meters'])
-
-IMAGE_COLLECTION = BaseImageCollection(
-    start_date = START_DATE,
-    end_date = END_DATE,
-    lon = LONGITUDE,
-    lat = LATITUDE,
-    collection = 'sentinel-2-l2a'
-)
-
-IMAGE_RESULTS = search_catalog(IMAGE_COLLECTION)
+FREQUENCY = CONFIG['frequency']
+ANNOTATE = CONFIG['annotate']
+BOUNDARY = CONFIG['boundary']
+ALL = CONFIG['all']
 
 if __name__ == "__main__":
-    start_time = time.time()
     log = logger(PROJECT_DIR / 'logs/eo.log')
     log.info('STARTED EO')
 
@@ -36,40 +26,34 @@ if __name__ == "__main__":
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
 
-    parser.add_argument("--mode", required=True, type=str) # Basic or histogram (currently disabled)
-    parser.add_argument("--freq", required=False, type=str) # Monthly, yearly, etc.
-    # Switches
-    parser.add_argument('--annt', default=False, action=argparse.BooleanOptionalAction) # Annotate images
-    parser.add_argument('--all', default=False, action=argparse.BooleanOptionalAction) # Export all assets (true color and bands)
-    parser.add_argument('--bdry', default=False, action=argparse.BooleanOptionalAction) # Plot boundaries
     # For histogram mode
+    parser.add_argument("--hist", required=False, type=str)
     parser.add_argument("--tif", required=False, type=str)
     parser.add_argument("--bn", required=False, type=str)
     parser.add_argument("--lo", required=False, type=str)
     parser.add_argument("--up", required=False, type=bool)
     args = parser.parse_args()
 
-    mode = args.mode
-    freq = args.freq
-    annt = args.annt
-    all = args.all
-    bdry = args.bdry
-
+    # NOTE # Temporarily disabled until AnnotatedImage is fully working
     # For histogram mode
+    # hist = args.hist
     # tif = args.tif
     # bn = args.bn
     # lo = args.lo
     # up = args.up
-
-    if mode == 'basic':
-        basic.run(
-            IMAGE_RESULTS, float(LONGITUDE), float(LATITUDE), float(BUFFER), frequency=freq,
-            annotate=annt, export_all=all, plot_boundary=bdry
-        ) 
-
-    elif mode == 'hist':
-        pass # Temporarily disable until AnnotatedImage is fully working
+    # if hist: 
         # hist.run(bn=bn, tif=tif, lo=lo, up=up, out=out)
 
-    end_time = time.time()
-    log.info(f"FINISHED IN {round(end_time-start_time, 2)} SECONDS")
+    log.info('CALLING VIA CLI')
+    basic_mode = BasicMode({
+        "start_date": START_DATE,
+        "end_date": END_DATE,
+        "latitude": LATITUDE,
+        "longitude": LONGITUDE,
+        "buffer": BUFFER,
+        "frequency": FREQUENCY,
+        "annotate": ANNOTATE,
+        "boundary": BOUNDARY,
+        "all": ALL
+    })
+    basic_mode.run()
--- eo/constants.py
+++ eo/constants.py
@@ -3,3 +3,15 @@ FREQUENCY_MAP = {
     'quarterly': 'QE',
     'yearly': 'YE'
 }
+
+REQUIRED_PARAMETERS = [
+    'start_date',
+    'end_date',
+    'latitude',
+    'longitude',
+    'buffer',
+    'frequency',
+    'annotate',
+    'boundary',
+    'all'
+]
